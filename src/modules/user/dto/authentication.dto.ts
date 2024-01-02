c98ae399db36a17f204746f3b5dfa247d7b17b39
Jaber Fathi <j.fathi1998@gmail.com>
2024-01-02 14:55:28 +0330

    feat:add-authentication-dto's

---
+++ src/modules/user/dto/authentication.dto.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { zodIdentifier } from "../model/identifier";
+import { zodPassword } from "../model/password";
+import { zodUsername } from "../model/username";
+import { isEmail } from "../model/email";
+
+export const signinDto = z.object({
+  identifier: zodIdentifier,
+  password: zodPassword,
+});
+
+export const signupDto = z.object({
+  username: zodUsername,
+  email: z.string().refine(isEmail),
+  passwords: zodPassword,
+});
+
+export const forgotPassDto = z.object({
+  identifier: zodIdentifier,
+});
+
+export const recoverPassDto = z.object({
+  token: z.string(),
+  password: zodPassword,
+});
