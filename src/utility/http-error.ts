f36e8dfa8837503fa08e47ad0c2889b8443922f8
Jaber Fathi <j.fathi1998@gmail.com>
2024-02-08 14:37:46 +0330

    add:add-diffrent-kinds-of-errors

--- src/utility/http-error.ts
+++ src/utility/http-error.ts
@@ -5,13 +5,37 @@ export class HttpError extends Error {
 }
 
 export class ForbiddenError extends HttpError {
-  constructor() {
-    super(403, "Forbidden");
+  constructor(message = "Forbidden") {
+    super(403, message);
   }
 }
 
 export class NotFoundError extends HttpError {
-  constructor() {
-    super(404, "Not found");
+  constructor(message = "Not found") {
+    super(404, message);
+  }
+}
+
+export class UnauthorizedError extends HttpError {
+  constructor(message = "Unauthorized") {
+    super(401, message);
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(message = "Bad request") {
+    super(400, message);
+  }
+}
+
+export class ConflictError extends HttpError {
+  constructor(message = "Conflict") {
+    super(409, message);
+  }
+}
+
+export class ServerError extends HttpError {
+  constructor(message = "Server error") {
+    super(500, message);
   }
 }
